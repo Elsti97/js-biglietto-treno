090772ec20768443610b5db1c75d33667ac7dc05
Elsti97 <elsti.shehati@hotmail.it>
2022-10-19 16:12:33 +0200

    Primi dati su js

---
+++ main.js
@@ -0,0 +1,13 @@
+// * Dati inziali
+
+let km = prompt ("Quanti km vuoi percorrere?");
+
+let età = prompt ("Quanti anni hai?");
+
+const prezzo_km = 0.21;
+
+// Prezzo base
+
+let prezzo_totale = km * prezzo_km;
+
+
